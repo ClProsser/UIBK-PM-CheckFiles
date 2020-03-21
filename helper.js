b2650b932b696a59109e6ed807858672c6592600
ClProsser <clemens.prosser@gmail.com>
2020-03-22 00:56:24 +0100

    Added support for zip files

--- helper.js
+++ helper.js
@@ -2,30 +2,19 @@ const fs = require("fs")
 const yaml = require("js-yaml")
 const sprintf = require("sprintf-js").sprintf
 const colors = require("colors")
+const unzipper = require('unzipper')
 
 let helper = {
     findConfig: path => {
-        let files = module.exports.allFiles(path)
+        let files = module.exports.allFiles(path, ["yml", "yaml"])
         if (files.length === 0) {
-            console.error("ERROR: This directory is empty".red)
+            console.error("ERROR: No .yaml / .yml file found (please specify configpath with '--config' parameter)".red)
+            process.exit(1)
+        } else if (files.length > 1) {
+            console.error("ERROR: There is more than one .yaml / .yml file. Please use the '--config' parameter!".red)
             process.exit(1)
         }
-        let config = "";
-        for (let i = 0; i < files.length; i++) {
-            let fileName = files[i]
-            if (["yml", "yaml"].includes(fileName.split('.').pop())) {
-                if (config !== "") {
-                    console.error("ERROR: There is more than one .yaml / .yml file. Please use the '--config' parameter!".red)
-                    process.exit(1)
-                }
-                config = fileName;
-            }
-        }
-        if (config === "") {
-            console.error("ERROR: No .yaml / .yml file found (please specify configpath with '--config' parameter).".red)
-            process.exit(1)
-        }
-        return config;
+        return files[0];
     },
     readConfig: path => {
         if (!module.exports.fileExists(path)) {
@@ -34,9 +23,7 @@ let helper = {
         }
         return yaml.safeLoad(fs.readFileSync(path, 'utf8'))
     },
-    checkFiles: config => {
-        const path = process.cwd()
-
+    checkFiles: (config, path) => {
         const tasks = Object.keys(config)
 
         for (let i = 0; i < tasks.length; i++) {
@@ -50,9 +37,15 @@ let helper = {
             }
         }
     },
-    allFiles: path => {
+    extractZip: (path, target) => {
+        return fs.createReadStream(path).pipe(unzipper.Extract({
+            path: target
+        })).promise();
+    },
+    allFiles: (path, extensions = []) => {
         try {
-            return fs.readdirSync(path);
+            let files = fs.readdirSync(path)
+            return extensions.length > 0 ? files.filter(fileName => extensions.includes(fileName.split('.').pop())) : files;
         } catch (err) {
             return []
         }
@@ -61,7 +54,7 @@ let helper = {
         try {
             return isFile ? fs.statSync(path).isFile() : fs.statSync(path).isDirectory()
         } catch (err) {
-            return false;
+            return false
         }
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
+
+const fs = require("fs")
 const helper = require("./helper")
 const yargs = require("yargs")
 const colors = require("colors")
+var rimraf = require("rimraf")
 
 const options = yargs
     .options({
@@ -31,10 +34,26 @@ if (!options.path) {
     }
 }
 
-
 if (!options.config) {
     options.config = helper.findConfig(options.path);
 }
 
 const config = helper.readConfig(options.config)
-helper.checkFiles(config)
+
+let zipFiles = helper.allFiles(options.path, ["zip", "gz"]);
+if (zipFiles.length === 1) {
+    console.log("Hint: Skipping folder as a zip exists".blue)
+    options.path += "/._temp_zip_" + Math.random().toString(36).substring(2);
+    helper.extractZip(zipFiles[0], options.path).then(() => {
+        helper.checkFiles(config, options.path)
+        rimraf(options.path, () => {});
+    }).catch(err => {
+        console.error(err)
+        process.exit(1)
+    });
+} else {
+    if (zipFiles.length > 1) {
+        console.log("Warning: Skipping .zip (there is more than one in this directory)".yellow)
+    }
+    helper.checkFiles(config, options.path)
+}
