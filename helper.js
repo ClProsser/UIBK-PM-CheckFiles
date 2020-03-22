b64f2cbc2c22084c188146f1ee3fb2e1d7cac5f9
ClProsser <clemens.prosser@gmail.com>
2020-03-22 21:05:08 +0100

    Added function descriptions and enhanced warnings

--- helper.js
+++ helper.js
@@ -1,74 +1,157 @@
-const fs = require("fs")
-const yaml = require("js-yaml")
-const sprintf = require("sprintf-js").sprintf
-const colors = require("colors")
+const fs = require('fs')
+const yaml = require('js-yaml')
+const sprintf = require('sprintf-js').sprintf
+const colors = require('colors')
 const unzipper = require('unzipper')
 
 let helper = {
+    /*
+     * Checks if there exists exactly one .yaml / .yml file
+     *
+     * @param string path
+     * @return string
+     */
     findConfig: path => {
-        let files = module.exports.allFiles(path, ["yml", "yaml"])
+        let files = module.exports.allFiles(path, false, ['yml', 'yaml'])
         if (files.length === 0) {
-            console.error("ERROR: No .yaml / .yml file found (please specify configpath with '--config' parameter)".red)
+            console.error('ERROR: No .yaml / .yml file found (please specify configpath with "--config" parameter)'.red)
             process.exit(1)
         } else if (files.length > 1) {
-            console.error("ERROR: There is more than one .yaml / .yml file. Please use the '--config' parameter!".red)
+            console.error('ERROR: There is more than one .yaml / .yml file. Please use the "--config" parameter!'.red)
             process.exit(1)
         }
         return files[0]
     },
+    /*
+     * Reads and parses config file
+     *
+     * @param string path
+     * @return object
+     */
     readConfig: path => {
         if (!module.exports.fileExists(path)) {
-            console.error("ERROR: The provided yaml file does not exist".red)
+            console.error('ERROR: The provided yaml file does not exist'.red)
             process.exit(1)
         }
         return yaml.safeLoad(fs.readFileSync(path, 'utf8'))
     },
-    checkFiles: (config, path, summarize) => {
+    /*
+     * Checks directory for file existence
+     *
+     * @param object config
+     * @param string path
+     * @param boolean summary
+     */
+    checkFiles: (config, path, summary) => {
         const tasks = Object.keys(config)
 
-        if (summarize) {
-            let found = 0
-            let missing = 0
-            for (let i = 0; i < tasks.length; i++) {
-                const task = tasks[i]
-                const requiredFiles = config[task]
-                for (let y = 0; y < requiredFiles.length; y++) {
-                    if (module.exports.fileExists(path + "/" + requiredFiles[y])) {
-                        found++
-                    } else {
-                        missing++
-                    }
+        let files = module.exports.allFiles(path, true)
+
+        let found = []
+        let missing = []
+
+        for (let i = 0; i < tasks.length; i++) {
+            const task = tasks[i]
+            const requiredFiles = config[task]
+
+            if (!summary) {
+                console.log(task + ':')
+            }
+            for (let y = 0; y < requiredFiles.length; y++) {
+                let file = requiredFiles[y]
+                let exists
+                if (files.includes(file)) {
+                    exists = 'OK'.green
+                    found.push(file)
+                } else {
+                    exists = 'MISSING'.red
+                    missing.push(file)
+                }
+                if (!summary) {
+                    console.log(sprintf('   %-64s %s', file, exists))
                 }
             }
-            let total = found + missing
-            console.log("Summary: " + colors.green(found) + "/" + total + " files found (" + (missing === 0 ? colors.green(missing) : colors.red(missing)) + " missing)")
-            console.log("Status: " + (found === total ? "Complete".green : "Files missing".red))
-        } else {
-            for (let i = 0; i < tasks.length; i++) {
-                const task = tasks[i]
-                const requiredFiles = config[task]
+        }
+        if (summary) {
+            found = found.filter((item, index) => found.indexOf(item) === index)
+            missing = missing.filter((item, index) => missing.indexOf(item) === index)
 
-                console.log(task + ":")
-                for (let y = 0; y < requiredFiles.length; y++) {
-                    let exists = module.exports.fileExists(path + "/" + requiredFiles[y]) ? "OK".green : "MISSING".red
-                    console.log(sprintf("   %-64s ", requiredFiles[y]), exists)
+            let total = found.length + missing.length
+            console.log('Summary: ' + colors.green(found.length) + '/' + total + ' files found (' + (missing.length === 0 ? colors.green(missing.length) : colors.red(missing.length)) + ' missing)')
+            console.log('Status: ' + (found.length === total ? 'Complete'.green : 'Files missing'.red))
+        }
+
+        console.log('')
+
+        let allRequiredFiles = tasks.map(key => config[key])
+        allRequiredFiles = [].concat.apply([], allRequiredFiles)
+
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i]
+
+            if (!allRequiredFiles.includes(file)) {
+                if ((file !== 'group.txt') && !(['yaml', '.yml'].includes(file.slice(-4)))) {
+                    console.log(colors.yellow('Waring: ' + file + ' is not required!'))
                 }
             }
         }
     },
+    /*
+     * Extracts zip file
+     *
+     * @param string path
+     * @param string target
+     * @return promise
+     */
     extractZip: (path, target) => {
         return fs.createReadStream(path).pipe(unzipper.Extract({
             path: target
         })).promise()
     },
-    allFiles: (path, extensions = []) => {
+    /*
+     * Returns all files in a directory
+     *
+     * @param string path
+     * @param boolean recursive
+     * @param array extensions
+     * @param string sub (DO NOT PASS)
+     * @return array
+     */
+    allFiles: (path, recursive = false, extensions = [], sub = '') => {
+        if (path.slice(-1) !== '/') {
+            path += '/'
+        }
+        const filter = extensions.length === 0
         try {
-            let files = fs.readdirSync(path)
-            return extensions.length > 0 ? files.filter(fileName => extensions.includes(fileName.split('.').pop())) : files
+            let allFiles = fs.readdirSync(path + sub)
+            let files = []
+            if (recursive) {
+                allFiles.forEach(file => {
+                    const relativePath = sub + file
+                    if (module.exports.fileExists(path + relativePath, false)) {
+                        files = files.concat(module.exports.allFiles(path, true, extensions, relativePath + '/'))
+                    } else {
+                        if (filter || extensions.includes(relativePath.split('.').pop())) {
+                            files.push(relativePath)
+                        }
+                    }
+                })
+            } else {
+                files = fs.readdirSync(path)
+                files = files.filter(fileName => module.exports.fileExists(path + '/' + fileName) && (filter || extensions.includes(fileName.split('.').pop())))
+            }
+            return files
         } catch (err) {
             return []
         }
     },
+    /*
+     * Checks if a file / or directory exists
+     *
+     * @param string path
+     * @param boolean isFile
+     * @return boolean
+     */
     fileExists: (path, isFile = true) => {
         try {
             return isFile ? fs.statSync(path).isFile() : fs.statSync(path).isDirectory()
--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 #!/usr/bin/env node
 
-const fs = require("fs")
-const helper = require("./helper")
-const yargs = require("yargs")
-const colors = require("colors")
-const rimraf = require("rimraf")
+const fs = require('fs')
+const helper = require('./helper')
+const yargs = require('yargs')
+const colors = require('colors')
+const rimraf = require('rimraf')
+const path = require('path')
 
 const options = yargs
     .options({
         'config': {
-            alias: "c",
+            alias: 'c',
             description: 'YAML config file path',
             type: 'string',
             defaultDescription: 'any .yaml in pwd'
         },
         'path': {
-            alias: "p",
+            alias: 'p',
             description: 'Path of folder to check',
             type: 'string',
             defaultDescription: 'current dir (pwd)'
         },
         'summary': {
-            alias: "s",
+            alias: 's',
             description: 'Summarize ouput messages',
             type: 'boolean',
             default: false
         }
     }).argv
 
-
 if (!options.path) {
     options.path = process.cwd()
 } else {
     if (!helper.fileExists(options.path, false)) {
-        console.error("ERROR: This folder does not exist".red)
+        console.error('ERROR: This folder does not exist'.red)
         process.exit(1)
     } else {
         process.chdir(options.path)
+        options.path = process.cwd()
     }
 }
 
@@ -46,10 +47,10 @@ if (!options.config) {
 
 const config = helper.readConfig(options.config)
 
-let zipFiles = helper.allFiles(options.path, ["zip", "gz"])
+let zipFiles = helper.allFiles(options.path, false, ['zip', 'gz'])
 if (zipFiles.length === 1) {
-    console.log("Hint: Skipping folder as a zip exists".blue)
-    options.path += "/._temp_zip_" + Math.random().toString(36).substring(2)
+    console.log('Hint: Skipping folder as a zip exists'.blue)
+    options.path += '/._temp_zip_' + Math.random().toString(36).substring(2)
     helper.extractZip(zipFiles[0], options.path).then(() => {
         helper.checkFiles(config, options.path, options.summary)
         rimraf(options.path, () => {})
@@ -59,7 +60,7 @@ if (zipFiles.length === 1) {
     })
 } else {
     if (zipFiles.length > 1) {
-        console.log("Warning: Skipping .zip (there is more than one in this directory)".yellow)
+        console.log('Warning: Skipping .zip (there is more than one in this directory)'.yellow)
     }
     helper.checkFiles(config, options.path, options.summary)
 }
